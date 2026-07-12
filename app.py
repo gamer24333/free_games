906ec602bd605df639ea203b0d2a5d657dea3eb5
gamer24333 <till.buehrer@outlook.de>
2026-07-12 11:41:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 KLASSEN_LISTE = [
     "Till", "Ben", "Matteo", "Louis", "Maxim", "Jonah P", "Jonah S", 
     "Mateo", "Hanna", "Emma", "Lia", "Mia", "Lena S", "Lena G", 
-    "Lena D", "Dasha", "Daniel", "Bennet", "Erik", "Roman", 
+    "Lena D", "Dasha", "Daniel", "Bennet", "Erik", "Roman", "Meike",
     "Janne", "Tom", "Levin", "Liam", "Tim", "Nathalie", "Richard"
 ]
 
