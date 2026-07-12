665fa40de4c142c516abb3aca7d23c1d10abc0c7
gamer24333 <till.buehrer@outlook.de>
2026-07-12 21:12:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_url or 'sqlite:///local_portal.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+# Sorgt dafür, dass die Tabellen auch auf Render (unter Gunicorn) automatisch erstellt werden
+with app.app_context():
+    db.create_all()
 
 # Feste Klassenliste
 KLASSEN_LISTE = [
