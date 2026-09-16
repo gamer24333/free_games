cc7e0e80af95acf8fb75b7f47404f1b9482234df
gamer24333 <till.buehrer@outlook.de>
2026-09-16 19:12:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,14 @@ class Feedback(db.Model):
     message = db.Column(db.Text, nullable=False)
     is_read = db.Column(db.Boolean, default=False)
 
+class AdminMessage(db.Model):
+    __tablename__ = 'admin_messages'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sender = db.Column(db.String(50), nullable=False)
+    target = db.Column(db.String(50), nullable=False) # 'alle' oder einzelner Name
+    title = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+
 class UserSetting(db.Model):
     __tablename__ = 'user_settings'
     username = db.Column(db.String(50), primary_key=True)
